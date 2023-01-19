497894a9c9adff3e10c6c5a5dbdfaca08e584285
Ioanna Apostolopoulou <61350678+Ioanna-Apostolopoulou@users.noreply.github.com>
2023-01-19 14:04:56 +0200

    Add files via upload

---
+++ assignment_3/assignment_3/askisi_3.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <omp.h>
+#include "header.h"
+
+
+int create_sparse_csr(const double* A, int n_rows, int n_cols, int n_nz, Sparse_CSR_t* A_csr) 
+{
+    
+    A_csr->n_rows = n_rows;
+    A_csr->n_cols = n_cols;
+    A_csr->n_nz = n_nz;
+    A_csr->row_ptrs = calloc(n_rows+1, sizeof(int));
+    A_csr->col_indices = calloc(n_nz, sizeof(int));
+    A_csr->values = calloc(n_nz, sizeof(double));
+
+    int nz_id = 0;
+    int nz_loc = 0;
+
+    
+    for (int i=0; i<n_rows; ++i) 
+    {
+        
+        #pragma omp critical (nz_id)
+            {
+               A_csr->row_ptrs[i] = nz_id;
+               printf("%d\n\n", nz_id);
+            }
+        #pragma omp parallel
+        #pragma omp for private(nz_loc)
+        for (int j=0; j<n_cols; ++j) 
+        {
+            if (A[i*n_cols + j] != 0.0) 
+            {
+                #pragma omp critical (nz_id)
+                {
+                    nz_loc = nz_id;
+                    nz_id++;
+
+                }
+                A_csr->col_indices[nz_loc] = j;
+                A_csr->values[nz_loc] = A[i*n_cols + j];
+            
+            }
+        }
+    }
+
+    A_csr->row_ptrs[n_rows] = nz_id;
+
+    return EXIT_SUCCESS;
+}
+
+int matrix_vector_sparse_csr(const Sparse_CSR_t* A_csr, const double* vec, double* res) 
+{
+    #pragma omp parallel 
+    #pragma omp for
+    for (int i=0; i<A_csr->n_rows; ++i) {
+        res[i] = 0.0;
+        int nz_start = A_csr->row_ptrs[i];
+        int nz_end = A_csr->row_ptrs[i+1];
+        for (int nz_id=nz_start; nz_id<nz_end; ++nz_id) {
+            int j = A_csr->col_indices[nz_id];
+            double val = A_csr->values[nz_id];
+            res[i] = res[i] + val * vec[j];
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int main (int argc, char** argv) 
+{
+    int n_rows = 5;
+    int n_cols = 5;
+    int n_nz = 12;
+
+    double A[] = {
+        1,  0,  0,  2,  0,
+        3,  4,  2,  5,  0,
+        5,  0,  0,  8, 17,
+        0,  0, 10, 16,  0,
+        0,  0,  0,   0, 14
+    };
+    double x[] = {
+        1,
+        2,
+        3,
+        4,
+        5
+    };
+    double Ax[5];
+
+    // Starts here
+    Sparse_CSR_t A_csr;
+
+    create_sparse_csr(A, n_rows, n_cols, n_nz, &A_csr);
+
+    print_sparse_csr(&A_csr);
+
+    matrix_vector_sparse_csr(&A_csr, x, Ax);
+
+    for (int i=0; i<n_rows; ++i) 
+    {
+        printf("%02.2f\n", Ax[i]);
+    }
+
+    free_sparse_csr(&A_csr);
+
+    return EXIT_SUCCESS;
+}
+
---
+++ assignment_3/assignment_3/header.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Sparse_CSR 
+{
+    int n_rows;
+    int n_cols;
+    int n_nz;
+    int* row_ptrs;
+    int* col_indices;
+    double* values;
+} Sparse_CSR_t;
+
+
+int print_sparse_csr(const Sparse_CSR_t* A_csr) 
+{
+    printf("row\tcol\tval\n");
+    printf("----\n");
+
+    for (int i=0; i<A_csr->n_rows; ++i) {
+        int nz_start = A_csr->row_ptrs[i];
+        int nz_end = A_csr->row_ptrs[i+1];
+
+        for (int nz_id=nz_start; nz_id<nz_end; ++nz_id) {
+            int j = A_csr->col_indices[nz_id];
+            double val = A_csr->values[nz_id];
+            printf("%d\t%d\t%02.2f\n", i, j, val);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int free_sparse_csr(Sparse_CSR_t* A_csr) 
+{
+    free(A_csr->row_ptrs);
+    free(A_csr->col_indices);
+    free(A_csr->values);
+
+    return EXIT_SUCCESS;
+}
